b2ac47f1d70ad8d859a21480e626c9f571ae5d99
Jack O'Brien <70115137+jackobrien44@users.noreply.github.com>
2026-04-26 21:29:30 -0500

    required

--- src/api/CableJack.Core/DTOs/LoginRequest.cs
+++ src/api/CableJack.Core/DTOs/LoginRequest.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CableJack.Core.DTOs
 {
     public sealed class LoginRequest
     {
+        [Required]
         public required string Username { get; set; }
+
+        [Required]
         public required string Password { get; set; }
     }
 }
--- src/api/CableJack.Core/DTOs/RegisterRequest.cs
+++ src/api/CableJack.Core/DTOs/RegisterRequest.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CableJack.Core.DTOs
 {
     public sealed class RegisterRequest
     {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
         public required string Username { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [MaxLength(100)]
         public required string Password { get; set; }
     }
 }
