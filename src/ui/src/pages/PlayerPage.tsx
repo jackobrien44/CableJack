4db7b925b814d294a8cd9befce40a28a50e71278
Jack O'Brien <70115137+jackobrien44@users.noreply.github.com>
2026-04-29 10:49:14 -0500

    misc fix

--- src/ui/src/pages/PlayerPage.tsx
+++ src/ui/src/pages/PlayerPage.tsx
@@ -131,7 +131,7 @@ export default function PlayerPage() {
                 >
                   <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
                     {showSidebar
-                      ? <path d="M3 18h13v-2H3v2zm0-5h10v-2H3v2zm0-7v2h13V6H3zm18 9.59L17.42 12 21 8.41 19.59 7l-5 5 5 5L21 15.59z"/>
+                      ? <path d="M3 18h13v-2H3v2zm0-5h10v-2H3v2zm0-7v2h13V6H3zm6 9.59L14.59 12 9 6.41 7.59 7.83 11.17 12l-3.58 4.17L9 17.59z"/>
                       : <path d="M3 18h13v-2H3v2zm0-5h10v-2H3v2zm0-7v2h13V6H3zm18 9.59L17.42 12 21 8.41 19.59 7l-5 5 5 5L21 15.59z"/>
                     }
                   </svg>
